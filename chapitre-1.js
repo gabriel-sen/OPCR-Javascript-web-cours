ef57a1cbd04a9206391965de9404f6e6d39423a7
gabriel <salut.gaby@gmail.com>
2018-08-26 22:01:16 +0200

    chaptre 1 le DOM - Git  +  .gitignore

---
+++ chapitre-1.js
@@ -0,0 +1,5 @@
+var h = document.head; // La variable "h" contient l'objet "heade" du Dom
+console.log(h);
+
+var b = document.head; // La variable "b" contient l'objet "body" du DOM
+console.log(b);
